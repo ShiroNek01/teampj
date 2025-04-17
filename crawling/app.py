47fe74721be7c9f48182753cfe06ab333dbbd4d5
ShiroNek01 <dev.shironek01@gmail.com>
2025-04-17 22:24:35 +0900

    a

--- crawling/app.py
+++ crawling/app.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options #네이버는 자동화된 접근을 막기에 브라우저처럼 보이게하는 설정의 헤더파일
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import chromedriver_autoinstaller
 
 chromedriver_autoinstaller.install()
@@ -23,8 +26,41 @@ search_input = driver.find_element(By.CLASS_NAME, "input_search")
 search_input.send_keys("괴정동 맛집")
 search_input.send_keys(Keys.ENTER)
 
-#결과 대기
-time.sleep(5)
+#searchIframe으로 이동
+try:
+    WebDriverWait(driver, 10).until(
+        EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe"))
+    )
+except:
+    print('에러')
+    driver.quit()
+    exit()
+#검색 결과 요소 가져오기
+try:
+    place_list = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "TYaxT"))
+    )
+    print(f"{len(place_list)}")
+except:
+    print("에러 발생")
+    driver.quit()
+    exit()
+
+place_info = {}
+
+if place_list:
+    for i in range(7):
+        place_name = place_list[i].text.strip()
+        place_info[place_name] = {"review": []}
+        
+        place_list[i].click()
+        time.sleep(2)
+        
+        driver.switch_to.default_content()
+        try:
+            driver.switch_to.frame("searchIframe")
+        except:
+            print("에러")
 
 
 input()
