a21fc48bc9f354daba4d11d76a68738bdf9a1222
ShiroNek01 <dev.shironek01@gmail.com>
2025-05-07 18:38:48 +0900

    006

--- crawling/crawler.py
+++ crawling/crawler.py
@@ -1,38 +1,39 @@
+#기본 라이브러리 및 Selenium 관련 모듈
 import time
 import random
 import json 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options #네이버는 자동화된 접근을 막기에 브라우저처럼 보이게하는 설정의 헤더파일
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options   #크롬 브라우저 설정 옵션
+from selenium.webdriver.common.by import By             #요소 탐색 기준 설정
+from selenium.webdriver.common.keys import Keys         #키보드 입력을 위한 모듈
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC    #요소 조건 명시
 
-import chromedriver_autoinstaller
+import chromedriver_autoinstaller   #사용자의 환경에 맞는 크롬드라이버 자동 설치
 
-chromedriver_autoinstaller.install()
+chromedriver_autoinstaller.install()    #크롬드라이버 설치 또는 버전 체크 후 설치
 
-#네이버는 자동화된 접근을 막기에 브라우저처럼 보이게하는 설정
+#크롬 옵션 설정(자동화된 접근으로 보이지 않기 위해)
 options = Options()
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+# options.add_argument("--headless") #백그라운드 실행 
 
+#사용자로 부터 검색어 입력받음
 search_place = input("검색어 입력 : ")
 
-# options.add_argument("--headless") #백그라운드 실행 
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+#크롬 드라이버 실행 및 네이버 지도로 이동
 driver = webdriver.Chrome(options=options)
+driver.get("https://map.naver.com/v5")
+time.sleep(random.uniform(2,4))     #로딩 대기
 
-#네이버 지도 열기
-driver.get("https://map.naver.com/v5") #get() 원하는 링크로 이동
-time.sleep(random.uniform(2,4))
-
-#검색창 요소 찾기
+#검색창 요소 input_search 찾고 검색 실행
 search_input = driver.find_element(By.CLASS_NAME, "input_search")
 search_input.send_keys(search_place)
 search_input.send_keys(Keys.ENTER)
 
 time.sleep(2)
 
-#searchIframe으로 이동
+#검색 결과가 있는 searchIframe으로 이동
 try:
     WebDriverWait(driver, 10).until(
         EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe"))
@@ -42,30 +43,33 @@ except:
     driver.quit()
     exit()
 
-place_info = {}
-index = 0
+place_info = {}     #결과를 저장할 딕셔너리
+index = 0   #스크롤 위치를 위한 인덱스
 
+#반복적으로 장소 정보 수집
 while True:
     #검색 결과 요소 가져오기
     try:
         place_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@class, 'TYaxT') or contains(@class, 'YwYLL')]"))
+            EC.presence_of_all_elements_located((By.XPATH, "//*[contains(@class, 'TYaxT') or contains(@class, 'YwYLL') or contains(@class, 'moQ_p')]"))
         )
-        print(f"{len(place_list)}")
+        print(f"{len(place_list)}") #찾은 요소 갯수
     except:
         print("요소 찾을 수 없음")
         driver.quit()
         exit()
 
-    end_index = min(index + 3, len(place_list))
+    end_index = min(index + 3, len(place_list)) #최대 3개의 장소까지만 수집함
 
     if place_list:
         for i in range(index, end_index):
             place_name = place_list[i].text.strip()
 
+            #중복된 장소 스킵
             if place_name in place_info:
                 continue
-
+            
+            #기본 데이터 구조
             place_info[place_name] = {
                 "review": [], #리뷰
                 "images": [], #이미지
@@ -75,7 +79,7 @@ while True:
                 "website":"" #홈페이지
                 }
 
-            #클릭
+            #상세정보 표기를 위해 클릭
             try:
                 click_target = place_list[i].find_element(By.XPATH, "./ancestor::div[contains(@class, 'place_bluelink')]")
                 click_target.click()
@@ -148,13 +152,14 @@ while True:
                 driver.quit()
                 exit()
 
+            #리뷰 수집의 상한 (현재 90개 까지만 수집한다.)
             max_review = 90
             collected = 0
             scroll_count = 0
             max_scroll = 10
 
             while True:
-                #리뷰 스크롤 내리기
+                #리뷰칸 스크롤 내리기
                 driver.execute_script("window.scrollBy(0, window.innerHeight);")
                 time.sleep(1)
 
@@ -170,10 +175,11 @@ while True:
                         collected += 1
                     if collected >= max_review:
                         break
+                #더 이상 스크롤할 수 없거나 최대 스크롤 횟수 도달 시 반복을 멈춤
                 if collected >= max_review:
                     break
                 
-                #더보기 클릭
+                #더보기 버튼이 있으면 클릭
                 try:
                     more_bt = driver.find_element(By.XPATH, "//a[@class='fvwqf']")
                     more_bt.click()
@@ -184,16 +190,16 @@ while True:
                     break
                 scroll_count += 1
 
+            #이미지 수집(최대 5개)
             img_list = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.XPATH, "//a[@class='place_thumb']/img"))
             )
 
-            #이미지 수집
             for img in img_list[:5]:
                 img_url = img.get_attribute("src")  # 이미지 URL 가져오기
                 place_info[place_name]["images"].append(img_url) 
 
-            #seacrhIframe으로 이동 (아직 entryIframe에 있기 때문)
+            #seacrhIframe으로 이동 (아직 entryIframe에 있기에)
             try:
                 driver.switch_to.default_content()
                 driver.switch_to.frame("searchIframe")
@@ -203,11 +209,15 @@ while True:
                 driver.quit()
                 exit()
             
-        result = json.dumps(place_info, ensure_ascii=False, indent=4)
-        print(result)
-        with open("result.json", "w", encoding="utf-8") as f:
+        #결과 출력 및 저장
+        result = json.dumps(place_info, ensure_ascii=False, indent=4) 
+        print(result) #출력
+        with open("result.json", "w", encoding="utf-8") as f:           #저장
             json.dump(place_info, f, ensure_ascii=False, indent=4)
 
+
+        #다음 3개 장소도 수집할지 여부 확인
+        #'y' 를 입력하면 반복문으로 돌아가고 'n'을 입력하면 프로그램 종료함.
         index = end_index
         print('=============================')
         more_info = input("continue? (y/n) : ")
--- crawling/filter.py
+++ crawling/filter.py
@@ -1,7 +1,15 @@
+# 형태소 분석기를 쓰지 않고 문장을 단어 단위로 나눌 수 있게하는 모듈
 from soynlp.tokenizer import RegexTokenizer
+
+#json 데이터 파일 읽기 용 모듈
 import json
+
+#테마별 출현 횟수를 세기 위해 필요함
 from collections import Counter
 
+#테마를 분류하기 위한 키워드
+#각 테마는 장소의 특징을 나타내며, 해당하는 키워드들을 리스트로 묶어놓음
+#이후 crawler.py 에서 수집한 리뷰 데이터에서 키워드의 출현 빈도에 따라 테마를 분류함
 keyword = {
     "힐링": ['자연', '산책', '풍경', '경치', '뷰', '공원', '야경', '조용', '편안', '휴식', '쉼', '명상', '여유', '숲'],
     "여름": ['바다', '해수욕', '물놀이', '해변', '모래사장', '계곡', '수영', '파라솔', '서핑'],
@@ -15,24 +23,39 @@ keyword = {
     "야경": ['야경', '불빛', '야시장', '조명', '전망대', '야간', '밤', '빛'],
 }
 
+#soynlp에서 제공하는 정규식 기반 토크나이저 초기화
+#기본 설정으로 한글 문장을 공백 기분이 아닌 정규식 기반으로 토큰화
 tokenizer = RegexTokenizer()
 
+
+#테마 추출 함수
 def extract(reviews):
-    theme_count = Counter()
+    theme_count = Counter()     #테마별 키워드 출현 횟수 저장
 
     for review in reviews:
+        #리뷰들을 토큰화하여 리스트로 반환
         tokens = tokenizer.tokenize(review, flatten=True)
+        
+        #그리고 각 테마에 대해 해당 키워드가 토큰 안에 존재하는지 확인
         for theme, keywords in keyword.items():
+            #tokens 내에 keywords 중 하나라도 포함되면 count 증가
             if any(kw in token for kw in keywords for token in tokens):
                 theme_count[theme] += 1
 
+    #출현 빈도 기준으로 내림차순 정렬하여 상위 3개 테마 추출함
     return [theme for theme, _ in theme_count.most_common(3)]
 
+#json 파일 result.json 불러옴
 with open('result.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
+#장소별 리뷰에 대해 테마 추출 및 데이터에 저장
+#themes라는 새 키를 추가하여 테마 분석 결과를 저장함
 for place_name, info in data.items():
+    #해당 장소의 리뷰 리스트를 추출하고 테마 분석
     themes = extract(info.get("review", []))
+    #themes 필드로 결과 저장
     data[place_name]["themes"] = themes
 
+#json 형식으로 출력
 print(json.dumps(data, ensure_ascii=False, indent=4))
